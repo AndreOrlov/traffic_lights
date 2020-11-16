fd0b89a68ecca248dd70318cc75b3ff3ccdf8eb5
Andre Orlov <to@andreorlov.ru>
2020-11-17 01:47:24 +0300

    Basic functional

--- lib/traffic_lights.ex
+++ lib/traffic_lights.ex
@@ -1,18 +1,50 @@
 defmodule TrafficLights do
-  @moduledoc """
-  Documentation for `TrafficLights`.
-  """
+  use GenServer
 
-  @doc """
-  Hello world.
+  def start_link(_opts) do
+    GenServer.start_link(__MODULE__, %{status: :red_light_walker}, name: __MODULE__)
+  end
 
-  ## Examples
+  def init(opts) do
+    {:ok, opts}
+  end
 
-      iex> TrafficLights.hello()
-      :world
+  def push_walker_button do
+    GenServer.call(__MODULE__, :push_walker_button)
+  end
 
-  """
-  def hello do
-    :world
+  def red_light_walker_on do
+    GenServer.call(__MODULE__, :red_light_walker_on)
+  end
+
+  def red_light_walker_off do
+    GenServer.call(__MODULE__, :red_light_walker_off)
+  end
+
+  def status_light_walker do
+    GenServer.call(__MODULE__, :status_light_walker)
+  end
+
+  # Server
+
+  def handle_call(:push_walker_button, _from, state) do
+    # TODO доделать задержку
+    IO.inspect({"push_walker_button", %{state | status: :green_lignt_walker}})
+    {:reply, %{status: :green_lignt_walker}, %{state | status: :green_lignt_walker}}
+  end
+
+  def handle_call(:red_light_walker_on, _from, state) do
+    IO.inspect({"red_light_walker_on", %{state | status: :red_light_walker}})
+    {:reply, %{status: :red_light_walker}, %{state | status: :red_light_walker}}
+  end
+
+  def handle_call(:red_light_walker_off, _from, state) do
+    IO.inspect({"red_light_walker_off", %{state | status: :green_lignt_walker}})
+    {:reply, %{status: :green_lignt_walker}, %{state | status: :green_lignt_walker}}
+  end
+
+  def handle_call(:status_light_walker, _from, state) do
+    IO.inspect({"status", state})
+    {:reply, state, state}
   end
 end
--- mix.exs
+++ mix.exs
@@ -14,6 +14,7 @@ defmodule TrafficLights.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
+      mod: {TrafficLightsApp, []},
       extra_applications: [:logger]
     ]
   end
